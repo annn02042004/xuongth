1c932815bda9a525eb141357ca6fc481babc1081
Nguyen Van An <nguyenan02042004@gmail.com>
2024-08-08 15:19:55 +0700

    Thông điệp commit của bạn

---
+++ resources/views/clients/test.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.client')
+
+@section('title')
+    Trang test
+@endsection
+
+@section('css')
+    <style>
+        .content {
+            background-color: red;
+        }
+    </style>
+    <link rel="stylesheet" href="style.css">
+@endsection
+
+@section('content')
+    <h1>111111</h1>
+@endsection
+
+@section('js')
+    <script src=""></script>
+    <script>
+        // Code script
+    </script>
+@endsection
